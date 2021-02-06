df07e84cb175fdad3947ed0188795cf0ad5d477f
junwheih <68602533+junwheih@users.noreply.github.com>
2021-02-06 17:30:42 +0800

    060221 - More basic eg

--- Python/Playground/Feb2021.py
+++ Python/Playground/Feb2021.py
@@ -2,6 +2,38 @@
 
 quit()
 
+#8 String: Put a word repeatedly in each of the slit between the letters
+# Eg: ABC would output `AABCBABCC`
+text = 'ABC'
+print(text.join(reversed(text)))
+
+#7 String: Print a mirror text without the need of for loop
+text = 'Abc' # Output should be 'AbccbA'
+print(text + text[::-1])
+
+#6 String: Palindrome
+# Palindrome: sequence of characters which reads the same backward as forward
+userInput = input('Enter a sentence/word: ') # Try racecar
+print(userInput == "".join(reversed(userInput))) # True
+print(userInput == userInput[::-1]) # True
+'''Explanation : Extended slice offers to put a “step” field as [start,stop,step], and giving no field as start and stop indicates default to 0 and string length respectively and “-1” denotes starting from end and stop at the start, hence reversing string.'''
+
+#5 List: Sum all the even number
+nums = [5, 1, 20, 88, 16, 17, 23, 25, 66, 45, 86, 72]
+sum = 0
+
+for i in nums:
+    if i%2 == 0: sum += i
+
+print(sum) # 348
+
+#4 If-Else: Leap year
+year = int(input('Year: '))
+if (year % 4 == 0 and year % 100 != 0 or year % 400 == 0):
+    print('{year} is a loop year.')
+else:
+    print('{year} is not a loop year.')
+
 #3 Modulus: Finding day of the week
 start = int(input("Starting day: "))
 length = int(input("Holiday length: "))
