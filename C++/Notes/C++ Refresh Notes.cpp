2aa9caee5c86707fe457c3b96aa2a9153c2fd6b9
junwheih <68602533+junwheih@users.noreply.github.com>
2021-02-13 21:07:30 +0800

    130221 - Hello World of C++

---
+++ C++/Notes/C++ Refresh Notes.cpp
@@ -0,0 +1,19 @@
+/* BASICS OF C++
+========================================
+Last updated: Sat, February 13, 2021 - 21:06
+*/
+
+/* Hello World
+========================================*/
+#include <iostream>
+
+using namespace std;
+
+void main() {
+    cout << "Hello World" << endl;
+}
+
+/* Drawing a shape
+========================================*/
+
+
