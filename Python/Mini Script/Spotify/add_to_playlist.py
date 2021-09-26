f7ce8bdf037daab20a40d4af18fde18c692bc724
junwheih <68602533+junwheih@users.noreply.github.com>
2021-09-26 13:39:25 +0800

    260921 - Add to playlist for spotify app

--- Python/Mini Script/Spotify/add_to_playlist.py
+++ Python/Mini Script/Spotify/add_to_playlist.py
@@ -8,9 +8,9 @@ from os import system, name
 # NOTE: To my future me: Make sure to set your browser to 80% and in queue mode & F11 full screen mode
 # Mine is in 1920 x 1080 pixel resolution, change accordingly
 
+action = ['up', 'up', 'up', 'right', 'down']
+
 # define our clear function
-
-
 def clear():
     # for windows
     if name == 'nt':
@@ -37,6 +37,9 @@ def on_press(key):
         pyautogui.hotkey('win', 'ctrl', 'right')
         time.sleep(0.5)
 
+        # Close the ads banner
+        pyautogui.click(1405, 285)
+
         # Just in case there is duplication for the previous song
         pyautogui.press('esc')
         time.sleep(0.25)
@@ -44,20 +47,22 @@ def on_press(key):
         pyautogui.rightClick(px, py)
 
         # 3 up -> right -> down
-        pyautogui.press(['up', 'up', 'up', 'right', 'down'])
+        pyautogui.press(action)
         pyautogui.press('enter')
 
         pyautogui.hotkey('win', 'ctrl', 'left')
 
 
-cmd = input('[ENTER TO START]')
+cmd = input('[APP (Y/N)?]: ')
 
-if cmd == '':
-    clear()
-    print('Program started... \nINS: Add song\nPAUSE: End the program')
-    # Collect events until released
-    with keyboard.Listener(on_press=on_press) as listener:
-        try:
-            listener.join()
-        except MyException as e:
-            print('{0} was pressed'.format(e.args[0]))
+if cmd.upper() == 'Y':
+    action = ['up', 'up', 'right', 'down']
+    
+clear()
+print('Program started... \nINS: Add song\nPAUSE: End the program')
+# Collect events until released
+with keyboard.Listener(on_press=on_press) as listener:
+    try:
+        listener.join()
+    except MyException as e:
+        print('{0} was pressed'.format(e.args[0]))
