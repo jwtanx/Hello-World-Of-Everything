958efc4f5485183b124e8bd19f6456db6e51fe81
jwtanx <55975997+jwtanx@users.noreply.github.com>
2023-03-22 18:34:52 +0800

    220323 - Python Validating URL

--- Python/Notes/Python Refresh Notes.py
+++ Python/Notes/Python Refresh Notes.py
@@ -1211,6 +1211,44 @@ url = 'https://www.github.com'
 webbrowser.open_new(url)
 webbrowser.open_new_tab(url)
 
+VALIDATING URL
+=====
+# Checking if the url is valid
+# https://stackoverflow.com/a/7160778
+import re
+regex = re.compile(
+        r'^(?:http|ftp)s?://' # http:// or https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+        r'localhost|' #localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+        r'(?::\d+)?' # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+
+print(re.match(regex, "http://www.example.com") is not None) # True
+print(re.match(regex, "example.com") is not None)            # False
+
+# https://stackoverflow.com/a/38020041
+from urllib.parse import urlparse
+
+a = 'http://www.cwi.nl:80/%7Eguido/Python.html'
+b = '/data/Python.html'
+c = 532
+d = u'dkakasdkjdjakdjadjfalskdjfalk'
+e = 'https://stackoverflow.com'
+
+def uri_validator(x):
+    try:
+        result = urlparse(x)
+        return all([result.scheme, result.netloc])
+    except:
+        return False
+
+print(uri_validator(a)) # True
+print(uri_validator(b)) # False
+print(uri_validator(c)) # False
+print(uri_validator(d)) # False
+print(uri_validator(e)) # True
+
 DISPLAY ALL THE METHODS AVAILABLE FOR VARIABLE
 =====
 dir(tmp)
