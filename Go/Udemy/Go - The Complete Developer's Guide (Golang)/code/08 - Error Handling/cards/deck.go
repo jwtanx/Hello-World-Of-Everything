08b4427bb37cb62b50ea2514274cf4b9242c4d23
jwtanx <55975997+jwtanx@users.noreply.github.com>
2023-11-20 21:31:30 +0800

    201123 - [Go] Read from file, error handling

---
+++ Go/Udemy/Go - The Complete Developer's Guide (Golang)/code/08 - Error Handling/cards/deck.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
+
+}
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return os.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	s := strings.Split(string(bs), ",")
+	return deck(s)
+}
---
+++ Go/Udemy/Go - The Complete Developer's Guide (Golang)/code/08 - Error Handling/cards/main.go
@@ -0,0 +1,6 @@
+package main
+
+func main() {
+	cards := newDeckFromFile("my_cards")
+	cards.print()
+}
